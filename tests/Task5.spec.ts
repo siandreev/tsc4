4d2a2c7fbbddc84efe618748fd6b22d814a3bd43
siandrev <andreev.sergey.i@yandex.ru>
2023-08-16 12:30:07 +0200

    fix: task5 fix

--- tests/Task5.spec.ts
+++ tests/Task5.spec.ts
@@ -31,12 +31,12 @@ describe('Task5', () => {
         });
     });
 
-    it('should deploy', async () => {
+    it('Simple seq', async () => {
         const res = await task5.getFib(1, 3);
         expect(res).toStrictEqual(['1', '1', '2']);
     });
 
-    it('should deploy', async () => {
+    it('Big nums', async () => {
         const res = await task5.getFib(201, 4);
         expect(res).toStrictEqual([
             '453973694165307953197296969697410619233826',
@@ -44,5 +44,25 @@ describe('Task5', () => {
             '1188518561323126046432205871807859915657177',
             '1923063428480944139667114773918309212080528'
         ]);
+    });
+
+    it('Empty seq', async () => {
+        const res = await task5.getFib(0, 0);
+        expect(res).toStrictEqual([]);
+    });
+
+    it('Empty seq 2', async () => {
+        const res = await task5.getFib(1, 0);
+        expect(res).toStrictEqual([]);
+    });
+
+    it('Only start num', async () => {
+        const res = await task5.getFib(0, 1);
+        expect(res).toStrictEqual(['0']);
+    });
+
+    it('Only start num 3', async () => {
+        const res = await task5.getFib(0, 3);
+        expect(res).toStrictEqual(['0', '1', '1']);
     });
 });
